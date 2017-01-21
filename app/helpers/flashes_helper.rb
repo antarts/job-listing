c9b16d7dc16e0c2678b5597dc7b237347d48b266
Antarts <m.dream@foxmail.com>
2017-01-21 17:40:31 +0800

    implement reasch function

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -8,4 +8,9 @@ module FlashesHelper
   def user_facing_flashes
     flash.to_hash.slice 'alert', 'notice', 'warning'
   end
+
+  def render_highlight_content(job, query_string)
+    excerpt_cont = excerpt(job.title, query_string, radius: 500)
+    highlight(excerpt_cont, query_string)
+  end
 end
